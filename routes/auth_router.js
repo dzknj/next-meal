989da94ec1c7d61565e92425682b44662e967c2b
David Michael Zalk <dzknj@outlook.com>
2016-06-06 12:16:27 -0700

    added basicHTTP middleware and also more clear error messages in auth router depending on error, or if wrong password, etc.

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Organization = require(__dirname + '/../models/organization');
 const jsonParser = require('body-parser').json();
+const basicHTTP = require(__dirname + '/../lib/basic_http');
 
 var router = module.exports = exports = express.Router();
 
@@ -18,4 +19,12 @@ router.post('/signup', jsonParser, (req, res) => {
     res.json({msg: 'user created!'});
   });
 });
-// router.get('/signin')
+router.get('/signin', basicHTTP, (req, res) => {
+  Organization.findOne({organizationName: req.auth.organizationName}, (err, user) => {
+    if (err) return res.status(500).json({msg: 'authentication failed, db error'});
+    if (!user) return res.status(500).json({msg: 'authentication failed, no user'});
+    if (!user.compareHash(req.auth.password)) return res.status(500).json({msg: 'authentication failed on compareHash'})
+    res.json({msg: 'authentication success'})
+  });
+  // res.end();
+});
